947ba6358203e8e235eeafc5936731b58d85b433
Gloria Talavera <gloria.tare@gmail.com>
2024-01-18 12:20:38 +0100

    Configure cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,18 @@
+import "dotenv/config";
+
+import cors from "cors";
 import express from "express";
+
+const corsOptions = {
+  origin: [process.env.ORIGIN_LOCAL!, process.env.ORIGIN_PROD!],
+  methods: "GET",
+  preflightContinue: false,
+  optionSuccessStatus: 204,
+};
 
 const app = express();
 app.disable("x-powered-by");
+
+app.use(cors(corsOptions));
 
 export default app;
