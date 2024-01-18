8fcaffd4eecd2cb2528e1bbbc2c38bc3fc9fa49b
Gloria Talavera <gloria.tare@gmail.com>
2024-01-18 16:19:48 +0100

    Create functionality to connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import "dotenv/config";
 
+import debug from "debug";
+import chalk from "chalk";
 import startServer from "./server/startServer.js";
+import connectToDatabase from "./database/connectToDatabase.js";
 
 const port = process.env.PORT ?? 4005;
 
-startServer(+port);
+const mongoDbUrl = process.env.MONGO_DB_URL;
+
+try {
+  await connectToDatabase(mongoDbUrl!);
+  debug(chalk.green("Connected to database"));
+
+  startServer(+port);
+} catch (error: unknown) {
+  debug(chalk.red("Error connecting to database"));
+  debug(chalk.red((error as Error).message));
+  process.exit(1);
+}
