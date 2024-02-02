5d51073a87b1d4b66fccbc06bfad5908c40b86a4
Gloria Talavera <gloria.tare@gmail.com>
2024-02-02 13:52:48 +0100

    Test getSimpsonById controller

--- src/mocks/simpsonsMocks.ts
+++ src/mocks/simpsonsMocks.ts
@@ -27,3 +27,15 @@ export const simpsonsMock: SimpsonStructure[] = [
     occupation: "Empresario CEO y presidente de Powell Motor Company",
   },
 ];
+
+export const simpsonMockById: SimpsonStructure = {
+  _id: simpsonIdMock,
+  name: "Marge Simpson",
+  background:
+    "Marjorie Marge B. Simpson ,  (de soltera Bouvier ; nacida el 19 de marzo  ), es la feliz ama de casa y madre de tiempo completo de la familia Simpson . Con su esposo Homer , tiene tres hijos: Bart , Lisa y Maggie Simpson . Marge es la fuerza moralista en su familia y, a menudo, proporciona una voz fundamental en medio de las payasadas de su familia al tratar de mantener el orden en la casa de los Simpson. Aparte de sus deberes en el hogar, Marge coqueteó brevemente con una serie de carreras que van desde oficial de policía hasta activista contra la violencia.",
+  image:
+    "https://res.cloudinary.com/dglqojivj/image/upload/v1682559694/simpsons/250px-Marge_Simpson_ivadwr.png",
+  gender: "Femenino",
+  status: "Vivo",
+  occupation: "Ama de casa",
+};
